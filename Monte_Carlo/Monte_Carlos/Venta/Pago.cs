ef033f45acb39a96b96794c8eb2453d38c5dae19
Yovany1998 <61251212+Yovany1998@users.noreply.github.com>
2020-06-24 09:04:16 -0600

    Venta

---
+++ Monte_Carlo/Monte_Carlos/Venta/Pago.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Monte_Carlos.Venta
+{
+    public partial class Pago : Form
+    {
+
+        Conexion conexion;
+        private int state;
+        private Ventas venta;
+
+        public Pago()
+        {
+            InitializeComponent();
+            venta = new Ventas();
+            conexion = new Conexion();
+        }
+
+        private void BtnGenerarPago_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Pago realizado", "Pago", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            this.Hide();
+            Generar_Venta ventana = new Generar_Venta();
+            ventana.Show();
+        }
+
+        private void Pago_Load(object sender, EventArgs e)
+        {
+            int Id = 0;
+
+            DataTable Datos = conexion.consulta(String.Format("SELECT idVenta as 'Numero De Venta',idProducto as 'Producto',precio as 'Precio',Cantidad,Descuento,Total FROM DetalleDeVenta  where idVenta = {0};", Id));
+            dgvVenta.DataSource = Datos;
+            dgvVenta.Refresh();
+        }
+    }
+}
--- Monte_Carlo/Monte_Carlos/Venta/Ventas.cs
+++ Monte_Carlo/Monte_Carlos/Venta/Ventas.cs
@@ -3,10 +3,233 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace Monte_Carlos.Venta
 {
     class Ventas
     {
+        private Conexion conexion;
+        private int idVenta;
+        private int idFactura;
+        private string idCliente;
+        private string idEmpleado;
+        private DateTime fecha;
+        private int idProducto;
+        private double precio;
+        private int cantidades;
+        private double total;
+        private MySqlException error;
+
+
+        public Ventas()
+        {
+            idCliente = "";
+            idEmpleado = "";
+            fecha = DateTime.Today;
+            idProducto = 0;
+            precio = 0.0;
+            cantidades = 0;
+            total = 0.0;
+            conexion = new Conexion();
+        }
+        public Ventas(string a, string b, int c, double d, int e, double t)
+        {
+            idCliente = a;
+            idEmpleado = b;
+            fecha = DateTime.Today;
+            idProducto = c;
+            precio = d;
+            cantidades = e;
+            total = t;
+            conexion = new Conexion();
+        }
+
+        public double Total
+        {
+            get
+            {
+                return total;
+            }
+            set
+            {
+                total = value;
+            }
+        }
+        public int IdVenta
+        {
+            get
+            {
+                return idVenta;
+            }
+            set
+            {
+                idVenta = value;
+            }
+        }
+        public int IdFactura
+        {
+            get
+            {
+                return idFactura;
+            }
+            set
+            {
+                idFactura = value;
+            }
+        }
+        public string IdCliente
+        {
+            get
+            {
+                return idCliente;
+            }
+            set
+            {
+                idCliente = value;
+            }
+        }
+
+        public string IdEmpleado
+        {
+            get
+            {
+                return idEmpleado;
+            }
+            set
+            {
+                idEmpleado = value;
+            }
+        }
+
+
+        public DateTime Fecha
+        {
+            get
+            {
+                return fecha;
+            }
+            set
+            {
+                fecha = value;
+            }
+        }
+        public int IdProducto
+        {
+            get
+            {
+                return idProducto;
+            }
+            set
+            {
+                idProducto = value;
+            }
+        }
+        public double Precio
+        {
+            get
+            {
+                return precio;
+            }
+            set
+            {
+                precio = value;
+            }
+        }
+        public int Cantidades
+        {
+            get
+            {
+                return cantidades;
+            }
+            set
+            {
+                cantidades = value;
+            }
+        }
+
+
+        public MySqlException Error
+        {
+            get { return error; }
+        }
+
+        public Boolean Insertar()
+        {
+
+            /*Inserta datos en la tabla de detalle de venta */
+            if (conexion.IUD(string.Format("insert into DetalleDeVenta(idVenta,idFactura,idProducto,precio,Cantidad,Descuento ,Total) value('{0}','{1}','{2}','{3}','{4}','{5}','{6}')", idVenta, idFactura, idProducto, precio, cantidades, total)))
+            {
+                return true;
+            }
+            else
+            {
+                error = conexion.Error;
+                return false;
+            }
+        }
+
+        public Boolean Venta()
+        {
+            /*Inserta datos en la tabla de venta */
+            if (conexion.IUD(string.Format("insert into Venta(idCliente,idEmpleado) value('{0}','{1}')", idCliente, idEmpleado)))
+            {
+                IdVenta = Convert.ToInt32(conexion.consulta(string.Format("SELECT MAX(idVenta) from Venta")).Rows[0][0].ToString());
+            }
+            else
+            {
+                error = conexion.Error;
+            }
+
+            if (conexion.IUD(string.Format("insert into Factura(IdEmpleado,idCliente) value('{0}','{1}')", idEmpleado, idCliente)))
+            {
+                IdFactura = Convert.ToInt32(conexion.consulta(string.Format("SELECT MAX(IdFactura) from Factura")).Rows[0][0].ToString());
+                return true;
+            }
+            else
+            {
+                //error = conexion.Error;
+                return false;
+            }
+        }
+
+
+            public Boolean Eliminar()
+            {
+
+                if (conexion.IUD(string.Format("DELETE FROM DetalleDeVenta WHERE idVenta='{0}'", IdVenta)))
+                {
+
+                }
+                else
+                {
+                    error = conexion.Error;
+                }
+
+
+                if (conexion.IUD(string.Format("DELETE FROM Venta WHERE idVenta='{0}'", IdVenta)))
+                {
+
+                }
+                else
+                {
+                    error = conexion.Error;
+                }
+
+                if (conexion.IUD(string.Format("DELETE FROM factura WHERE IdFactura='{0}'", IdFactura)))
+                {
+                    // MessageBox.Show("La venta ha sido cancelada", " ", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                    return true;
+                }
+                else
+                {
+                    error = conexion.Error;
+                    return false;
+                }
+            }
+
+
+        }
     }
-}

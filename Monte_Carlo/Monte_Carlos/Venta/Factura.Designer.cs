59b148dc83828c4eb09268e2b773fe0064ea1765
Yovany1998 <61251212+Yovany1998@users.noreply.github.com>
2020-07-05 13:10:10 -0600

    Arreglos en factura

--- Monte_Carlo/Monte_Carlos/Venta/Factura.Designer.cs
+++ Monte_Carlo/Monte_Carlos/Venta/Factura.Designer.cs
@@ -33,6 +33,9 @@
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cmbdetalle = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Btndetalle = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvFactura)).BeginInit();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -42,11 +45,10 @@
             // dgvFactura
             // 
             this.dgvFactura.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvFactura.Location = new System.Drawing.Point(28, 150);
-            this.dgvFactura.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgvFactura.Location = new System.Drawing.Point(21, 122);
             this.dgvFactura.Name = "dgvFactura";
             this.dgvFactura.RowHeadersWidth = 51;
-            this.dgvFactura.Size = new System.Drawing.Size(820, 377);
+            this.dgvFactura.Size = new System.Drawing.Size(615, 306);
             this.dgvFactura.TabIndex = 44;
             this.dgvFactura.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvFactura_CellContentClick);
             // 
@@ -58,17 +60,18 @@
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(872, 70);
+            this.panel1.Size = new System.Drawing.Size(654, 57);
             this.panel1.TabIndex = 45;
             // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Bernard MT Condensed", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(284, 0);
+            this.label1.Location = new System.Drawing.Point(213, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(328, 70);
+            this.label1.Size = new System.Drawing.Size(246, 57);
             this.label1.TabIndex = 2;
             this.label1.Text = "Historial de facturas";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -77,9 +80,9 @@
             // 
             this.pictureBox2.Image = global::Monte_Carlos.Properties.Resources.conjunto_ilustracion_logotipo_restaurante_vintage_23_2148470301;
             this.pictureBox2.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(115, 70);
+            this.pictureBox2.Size = new System.Drawing.Size(86, 57);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
@@ -87,23 +90,57 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Monte_Carlos.Properties.Resources._64497;
-            this.pictureBox1.Location = new System.Drawing.Point(772, 0);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(579, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 70);
+            this.pictureBox1.Size = new System.Drawing.Size(75, 57);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.btnReturn_Click);
             // 
+            // cmbdetalle
+            // 
+            this.cmbdetalle.FormattingEnabled = true;
+            this.cmbdetalle.Location = new System.Drawing.Point(235, 77);
+            this.cmbdetalle.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbdetalle.Name = "cmbdetalle";
+            this.cmbdetalle.Size = new System.Drawing.Size(100, 21);
+            this.cmbdetalle.TabIndex = 46;
+            this.cmbdetalle.SelectedIndexChanged += new System.EventHandler(this.cmbdetalle_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(29, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(201, 18);
+            this.label3.TabIndex = 47;
+            this.label3.Text = "Historial segun la factura:";
+            // 
+            // Btndetalle
+            // 
+            this.Btndetalle.BackColor = System.Drawing.Color.Peru;
+            this.Btndetalle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btndetalle.Location = new System.Drawing.Point(425, 63);
+            this.Btndetalle.Name = "Btndetalle";
+            this.Btndetalle.Size = new System.Drawing.Size(131, 45);
+            this.Btndetalle.TabIndex = 48;
+            this.Btndetalle.Text = "Ver todos los detalles";
+            this.Btndetalle.UseVisualStyleBackColor = false;
+            this.Btndetalle.Click += new System.EventHandler(this.Btndetalle_Click);
+            // 
             // Factura
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(872, 554);
+            this.ClientSize = new System.Drawing.Size(654, 450);
+            this.Controls.Add(this.Btndetalle);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbdetalle);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.dgvFactura);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Factura";
             this.Text = "Factura";
             this.Load += new System.EventHandler(this.Factura_Load);
@@ -112,6 +149,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -121,5 +159,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox cmbdetalle;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Btndetalle;
     }
 }
--- Monte_Carlo/Monte_Carlos/Venta/Factura.cs
+++ Monte_Carlo/Monte_Carlos/Venta/Factura.cs
@@ -12,6 +12,7 @@ namespace Monte_Carlos.Venta
 {
     public partial class Factura : Form
     {
+        int contador;
         Conexion conexion;
 
         public Factura()
@@ -34,9 +35,51 @@ namespace Monte_Carlos.Venta
 
         private void Factura_Load(object sender, EventArgs e)
         {
-            DataTable Datos = conexion.consulta(String.Format("SELECT a.IdFactura,a.FechaActual as 'Fecha de venta',b.nombre as 'Nombre del cliente',c.nombre as 'Nombre del empleado' FROM ventas_comedor.factura a join ventas_comedor.cliente b on a.idCliente =b.idCliente  join  ventas_comedor.empleado c on c.idEmpleado =a.IdEmpleado;"));
+            llenarFactura();
+           // DataTable Datos = conexion.consulta(String.Format("SELECT a.IdFactura,a.FechaActual as 'Fecha de venta',b.nombre as 'Nombre del cliente',c.nombre as 'Nombre del empleado', d.total as 'Total del detalle de venta',sum(total) FROM ventas_comedor.factura a join ventas_comedor.cliente b on a.idCliente =b.idCliente  join  ventas_comedor.empleado c on c.idEmpleado =a.IdEmpleado join  ventas_comedor.detalledefactura d on d.idFactura = a.idFactura;"));
+            //dgvFactura.DataSource = Datos;
+            //dgvFactura.Refresh();
+        }
+
+        private void llenarFactura()
+        {
+            DataTable comida = new DataTable();
+            Conexion nuevo = new Conexion();
+            comida = nuevo.consulta(string.Format("SELECT * FROM ventas_comedor.factura"));
+            cmbdetalle.DataSource = null;
+            cmbdetalle.Items.Clear();
+            cmbdetalle.ValueMember = comida.Columns["idfactura"].ColumnName;
+            cmbdetalle.DisplayMember = comida.Columns["idfactura"].ColumnName;
+            cmbdetalle.DataSource = comida;
+            cmbdetalle.SelectedIndex = -1;
+            cmbdetalle.Text = "Selecciona una opcion";
+
+        }
+
+        private void cmbdetalle_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            contador = contador + 1;
+
+
+            if (contador > 2)
+            {
+                DataTable Datos = conexion.consulta(String.Format("SELECT a.IdFactura,a.FechaActual as 'Fecha de venta',b.nombre as 'Nombre del cliente',c.nombre as 'Nombre del empleado',sum(total) as 'Total a pagar de la venta' FROM ventas_comedor.factura a join ventas_comedor.cliente b on a.idCliente =b.idCliente  join  ventas_comedor.empleado c on c.idEmpleado =a.IdEmpleado join  ventas_comedor.detalledefactura d on d.idFactura = a.idFactura where a.idFactura ={0};",cmbdetalle.Text));
+                dgvFactura.DataSource = Datos;
+                dgvFactura.Refresh();
+               // contador = 1;
+
+
+
+            }
+        }
+
+        private void Btndetalle_Click(object sender, EventArgs e)
+        {
+            DataTable Datos = conexion.consulta(String.Format("SELECT a.IdFactura,a.FechaActual as 'Fecha de venta',b.nombre as 'Nombre del cliente',c.nombre as 'Nombre del empleado',d.total as 'Total detalle' FROM ventas_comedor.factura a join ventas_comedor.cliente b on a.idCliente =b.idCliente  join  ventas_comedor.empleado c on c.idEmpleado =a.IdEmpleado join  ventas_comedor.detalledefactura d on d.idFactura = a.idFactura;"));
             dgvFactura.DataSource = Datos;
             dgvFactura.Refresh();
+            // contador = 1;
+
         }
     }
 }
